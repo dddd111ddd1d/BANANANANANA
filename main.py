93f7c2c1c63ff05d465239cf39b1977a67527901
dddd111ddd1d <125788795+dddd111ddd1d@users.noreply.github.com>
2023-03-19 08:07:01 +0000

    klava

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://www.amazon.com/Autom-Trinket-Tray-Banana-Shaped/dp/B08DVCLKFK/ref=sr_1_2?crid=CL7H2UL2Q9IS&keywords=banana+trinket&qid=1679212807&sr=8-2"
+URL = "https://www.amazon.com/Razer-Huntsman-Mini-Gaming-Keyboard/dp/B08B3MHYPC/ref=sr_1_16?crid=2CP4WCWO2D6PC&keywords=gaming+keyboard&qid=1679213039&sprefix=gaming+ceyboard%2Caps%2C238&sr=8-16"
 
 res = requests.get(URL)
 
 if res.status_code == 200:
   soup = BeautifulSoup(res.text, features="html.parser")
-  info = soup.find_all("a", {"href":"/currencies/bitcoin/markets/"})
+  info = soup.find_all("span", {"class":"a-price a-text-price a-size-medium apexPriceToPay"})
   price = info[0].getText()
-  print("bitcoin -", price)
+  print("klava -", price)
  
   
   
