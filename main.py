23e20feb758755ba5f4d3bb8c6e152dc29d51b28
dddd111ddd1d <125788795+dddd111ddd1d@users.noreply.github.com>
2023-03-19 08:04:26 +0000

    sa

--- main.py
+++ main.py
@@ -1,27 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://coinmarketcap.com/"
+URL = "https://www.amazon.com/Autom-Trinket-Tray-Banana-Shaped/dp/B08DVCLKFK/ref=sr_1_2?crid=CL7H2UL2Q9IS&keywords=banana+trinket&qid=1679212807&sr=8-2"
 
 res = requests.get(URL)
 
 if res.status_code == 200:
   soup = BeautifulSoup(res.text, features="html.parser")
-  info = soup.find_all("a", {"href":"/currencies/ethereum/markets/"})
+  info = soup.find_all("a", {"href":"/currencies/bitcoin/markets/"})
   price = info[0].getText()
-  print("ethereum -", price)
-  info2 = soup.find_all("a", {"href":"/currencies/bitcoin/markets/"})
-  price = info2[0].getText()
   print("bitcoin -", price)
-  info3 = soup.find_all("a", {"href":"/currencies/tether/markets/"})
-  price = info3[0].getText()
-  print("tether -", price)
-  info4 = soup.find_all("a", {"href":"/currencies/bnb/markets/"})
-  price = info4[0].getText()
-  print("bnb -", price)
-  info5 = soup.find_all("a", {"href":"/currencies/xrp/markets/"})
-  price = info5[0].getText()
-  print("xpr -", price)
  
   
   
