bdf13be4be4e7312b08f99136f08181f0248b6a3
dddd111ddd1d <125788795+dddd111ddd1d@users.noreply.github.com>
2023-03-19 07:48:32 +0000

    stack overflow

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ res = requests.get(URL)
 
 if res.status_code == 200:
   soup = BeautifulSoup(res.text, features="html.parser")
-  info = soup.find_all("a", {"href":"/currencies/bitcoin/markets/"})
-  print(type(info[0]))
-  price = info[0].split("<span>")
-  print(price)
+  info = soup.find_all("a", {"href":"/currencies/ethereum/markets/"})
+  price = info[0].getText()
+  print("ethereum -", price)
+
+  
+  
