7dc1c5cb5ca2eca211cda3ba3e7fa597ace00813
dddd111ddd1d <125788795+dddd111ddd1d@users.noreply.github.com>
2023-03-19 09:02:20 +0000

    added product name and product price

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://www.amazon.com/Razer-Huntsman-Mini-Gaming-Keyboard/dp/B08B3MHYPC/ref=sr_1_16?crid=2CP4WCWO2D6PC&keywords=gaming+keyboard&qid=1679213039&sprefix=gaming+ceyboard%2Caps%2C238&sr=8-16"
+URL = "https://www.olx.ua/d/uk/obyavlenie/groviy-pk-i5-gtx660-12gb-ram-ssd-hdd-IDRmIKz.html"
 
 res = requests.get(URL)
 
 if res.status_code == 200:
   soup = BeautifulSoup(res.text, features="html.parser")
-  info = soup.find_all("span", {"class":"a-price a-text-price a-size-medium apexPriceToPay"})
-  price = info[0].getText()
-  print("klava -", price)
+  product_name = soup.find_all("h1", {"class":"css-1soizd2 er34gjf0"})
+  product_price = soup.find_all("h3", {"class":"css-ddweki er34gjf0"})
+  price = product_price[0].getText()
+  productName = product_name[0].getText()
+  print(productName, "-", price)
+
  
   
   
